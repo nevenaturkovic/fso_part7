118d13c4e6affa0055dfd345592ba25e2db7c866
Nevena Turkovic <nevenatu@gmail.com>
2022-05-16 18:44:11 +0200

    Finished 7.7: country hook

--- country-hook/src/App.js
+++ country-hook/src/App.js
@@ -1,27 +1,27 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState } from "react"
+import { useField, useCountry } from "./hooks/index.js"
 
-const useField = (type) => {
-  const [value, setValue] = useState('')
+// const useField = (type) => {
+//   const [value, setValue] = useState("")
 
-  const onChange = (event) => {
-    setValue(event.target.value)
-  }
+//   const onChange = (event) => {
+//     setValue(event.target.value)
+//   }
 
-  return {
-    type,
-    value,
-    onChange
-  }
-}
+//   return {
+//     type,
+//     value,
+//     onChange,
+//   }
+// }
 
-const useCountry = (name) => {
-  const [country, setCountry] = useState(null)
+// const useCountry = (name) => {
+//   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+//   useEffect(() => {})
 
-  return country
-}
+//   return country
+// }
 
 const Country = ({ country }) => {
   if (!country) {
@@ -31,16 +31,20 @@ const Country = ({ country }) => {
   return (
     <div>
       <h3>{country.name.common}</h3>
-      <div>population {country.population}</div> 
       <div>capital {country.capital}</div>
-      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/> 
+      <div>population {country.population}</div>
+      <img
+        src={country.flags.png}
+        height="100"
+        alt={`flag of ${country.name.common}`}
+      />
     </div>
-  )  
+  )
 }
 
 const App = () => {
-  const nameInput = useField('text')
-  const [name, setName] = useState('')
+  const nameInput = useField("text")
+  const [name, setName] = useState("")
   const country = useCountry(name)
 
   const fetch = (e) => {
